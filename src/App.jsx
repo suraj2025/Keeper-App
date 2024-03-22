b36a0232d5b00a55f81bb8fa0c3ae86863cd2dda
Suraj Kumar <118501104+suraj2025@users.noreply.github.com>
2024-03-22 13:44:36 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -3,36 +3,47 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Note from "./components/Note";
 import CreateArea from "./components/CreateArea";
-
+const initialState = [{
+  id:1,
+  title: "Demo",
+  content: "This is my first Note"
+}]
 function App() {
-  const [notes, setNotes] = useState([]);
-
+  const [notes, setNotes] = useState(initialState);
+  const [editableNote, setEditableNote] = useState(null)
   function addNote(newNote) {
-    setNotes(prevNotes => {
-      return [...prevNotes, newNote];
-    });
+    setNotes([
+      ...notes,
+      {...newNote, id: notes.length+1}
+    ]);
   }
-
   function deleteNote(id) {
-    setNotes(prevNotes => {
-      return prevNotes.filter((noteItem, index) => {
-        return index !== id;
-      });
-    });
+    setNotes(notes.filter(v=>v.id !== id));
   }
+  function updateNote(id){
+    setEditableNote(notes.find(v=> v.id === id));
+  }
+   function update(note){
+    const idx=notes.findIndex(n => n.id === note.id)
+    const newNotes=[...notes]
+    newNotes.splice(idx,1,note)
+    setNotes(newNotes)
+    setEditableNote(null)
+   }
 
   return (
     <div>
       <Header />
-      <CreateArea onAdd={addNote} />
-      {notes.map((noteItem, index) => {
+      <CreateArea onAdd={addNote} edit={editableNote} update={update} />
+      {notes.map((note) => {
         return (
           <Note
-            key={index}
-            id={index}
-            title={noteItem.title}
-            content={noteItem.content}
+            key={note.id}
+            id={note.id}
+            title={note.title}
+            content={note.content}
             onDelete={deleteNote}
+            editNote={updateNote}
           />
         );
       })}
